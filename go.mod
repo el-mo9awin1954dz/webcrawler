a986ec597b1c8bf907d37e2e241096122f226bbb
el-mo9awin1954dz <118679598+el-mo9awin1954dz@users.noreply.github.com>
2023-02-28 13:11:48 +0100

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,24 @@
+module webcrawler
+
+go 1.19
+
+require (
+	github.com/dimiro1/banner v1.1.0
+	github.com/dineshsprabu/concurrent-web-crawler v0.0.0-20170505055956-d21717df7b11
+	github.com/gookit/color v1.5.2
+	github.com/logrusorgru/aurora v2.0.3+incompatible
+	github.com/martinlindhe/notify v0.0.0-20181008203735-20632c9a275a
+	github.com/mattn/go-colorable v0.1.13
+	github.com/raphamorim/go-rainbow v0.1.0
+)
+
+require (
+	github.com/common-nighthawk/go-figure v0.0.0-20200609044655-c4b36f998cf2 // indirect
+	github.com/deckarep/gosx-notifier v0.0.0-20180201035817-e127226297fb // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+	github.com/tomnomnom/xtermcolor v0.0.0-20160428124646-b78803f00a7e // indirect
+	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	gopkg.in/toast.v1 v1.0.0-20180812000517-0a84660828b2 // indirect
+)
---
+++ webcrawler.go
@@ -0,0 +1,137 @@
+package main
+
+import(
+"github.com/dineshsprabu/concurrent-web-crawler"
+"os"
+alert  "github.com/gookit/color"
+"github.com/martinlindhe/notify"
+"fmt"
+"bufio"
+"bytes"
+"strconv"
+"flag"
+"github.com/raphamorim/go-rainbow"
+"github.com/dimiro1/banner"
+"github.com/mattn/go-colorable"
+"github.com/logrusorgru/aurora"
+)
+
+func usage() {
+
+	
+    fmt.Fprintf(os.Stderr, "usage: ./webcrawler [file-url-list] [max-req] [delay-req] -save [SAVE-PATH-FOLDER]\n")
+    flag.PrintDefaults()
+    os.Exit(2)
+}
+
+func main(){
+	// Creating a web crawler object with configurations.
+
+
+	flag.Usage = usage
+    	flag.Parse()
+
+    	args := flag.Args()
+    	
+	if len(args) < 4 {
+
+		who := "ELMO9AWIMDZ"
+
+		notify.Alert("WEBCRAWLER", "SETUP AND USAGE",who, "bug.png")
+								
+
+		isEnabled := true
+  		isColorEnabled := true
+		logo := `
+{{.AnsiColor.BrightBlue}}████████████████████████████████████████████████████████████████████████████████
+{{.AnsiColor.BrightBlue}}████████████████████████████████████████████████████████████████████████████████
+{{.AnsiColor.Red}}██████████████████{{.AnsiColor.BrightBlue}}████████████████{{.AnsiColor.Black}}██████████████████████████████{{.AnsiColor.BrightBlue}}████████████████
+{{.AnsiColor.Red}}████████████████████████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██████████████████████████████{{.AnsiColor.Black}}██{{.AnsiColor.BrightBlue}}██████████████
+{{.AnsiColor.BrightRed}}████{{.AnsiColor.Red}}██████████████████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██████{{.AnsiColor.Magenta}}██████████████████████{{.AnsiColor.White}}██████{{.AnsiColor.Black}}██{{.AnsiColor.BrightBlue}}████████████
+{{.AnsiColor.BrightRed}}██████████████████████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}████{{.AnsiColor.Magenta}}████████████████{{.AnsiColor.Black}}████{{.AnsiColor.Magenta}}██████{{.AnsiColor.White}}████{{.AnsiColor.Black}}██{{.AnsiColor.BrightBlue}}██{{.AnsiColor.Black}}████{{.AnsiColor.BrightBlue}}██████
+{{.AnsiColor.BrightRed}}██████████████████████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██{{.AnsiColor.Magenta}}████████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}████{{.AnsiColor.Black}}██{{.AnsiColor.Magenta}}██████{{.AnsiColor.White}}██{{.AnsiColor.Black}}████{{.AnsiColor.White}}████{{.AnsiColor.Black}}██{{.AnsiColor.BrightBlue}}████
+{{.AnsiColor.BrightYellow}}██████████████████{{.AnsiColor.BrightRed}}████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██{{.AnsiColor.Magenta}}████████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██████{{.AnsiColor.Magenta}}██████{{.AnsiColor.White}}██{{.AnsiColor.Black}}██{{.AnsiColor.White}}██████{{.AnsiColor.Black}}██{{.AnsiColor.BrightBlue}}████
+{{.AnsiColor.BrightYellow}}██████████████████████{{.AnsiColor.Black}}██{{.AnsiColor.BrightYellow}}██████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██{{.AnsiColor.Magenta}}████████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██████{{.AnsiColor.Black}}████████{{.AnsiColor.White}}████████{{.AnsiColor.Black}}██{{.AnsiColor.BrightBlue}}████
+{{.AnsiColor.BrightYellow}}████████████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██{{.AnsiColor.Black}}██{{.AnsiColor.BrightYellow}}████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██{{.AnsiColor.Magenta}}████████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██████████████████████{{.AnsiColor.Black}}██{{.AnsiColor.BrightBlue}}████
+{{.AnsiColor.BrightGreen}}██████████████████{{.AnsiColor.BrightYellow}}██{{.AnsiColor.Black}}██{{.AnsiColor.White}}██{{.AnsiColor.Black}}████████{{.AnsiColor.White}}██{{.AnsiColor.Magenta}}██████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██████████████████████████{{.AnsiColor.Black}}██{{.AnsiColor.BrightBlue}}██
+{{.AnsiColor.BrightGreen}}██████████████████████{{.AnsiColor.White}}████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██{{.AnsiColor.Magenta}}██████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██████{{.AnsiColor.BrightYellow}}██{{.AnsiColor.White}}██████████{{.AnsiColor.BrightYellow}}██{{.AnsiColor.Black}}██{{.AnsiColor.White}}████{{.AnsiColor.Black}}██{{.AnsiColor.BrightBlue}}██
+{{.AnsiColor.BrightGreen}}██████████████████████{{.AnsiColor.Black}}████{{.AnsiColor.White}}████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██{{.AnsiColor.Magenta}}██████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██{{.AnsiColor.Black}}████{{.AnsiColor.White}}████{{.AnsiColor.Black}}██{{.AnsiColor.BrightBlue}}██
+{{.AnsiColor.Blue}}██████████████████{{.AnsiColor.BrightGreen}}████████{{.AnsiColor.Black}}██████{{.AnsiColor.White}}██{{.AnsiColor.Magenta}}██████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██{{.AnsiColor.Magenta}}████{{.AnsiColor.White}}████████████████{{.AnsiColor.Magenta}}████{{.AnsiColor.Black}}██{{.AnsiColor.BrightBlue}}██
+{{.AnsiColor.Blue}}██████████████████████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}████{{.AnsiColor.Magenta}}██████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██████{{.AnsiColor.Black}}████████████{{.AnsiColor.White}}████{{.AnsiColor.Black}}██{{.AnsiColor.BrightBlue}}████
+{{.AnsiColor.BrightBlue}}██████████████████{{.AnsiColor.Blue}}████{{.AnsiColor.Blue}}██████{{.AnsiColor.Black}}████{{.AnsiColor.White}}██████{{.AnsiColor.Magenta}}██████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██████████████████{{.AnsiColor.Black}}██{{.AnsiColor.BrightBlue}}██████
+{{.AnsiColor.BrightBlue}}██████████████████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██{{.AnsiColor.Black}}████{{.AnsiColor.White}}████████████████████{{.AnsiColor.Black}}██████████████████{{.AnsiColor.BrightBlue}}████████
+{{.AnsiColor.BrightBlue}}████████████████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}██████{{.AnsiColor.Black}}████████████████████████████████{{.AnsiColor.White}}██{{.AnsiColor.Black}}██{{.AnsiColor.BrightBlue}}████████████
+{{.AnsiColor.BrightBlue}}████████████████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}████{{.AnsiColor.Black}}██{{.AnsiColor.BrightBlue}}██{{.AnsiColor.Black}}██{{.AnsiColor.White}}████{{.AnsiColor.BrightBlue}}████████████{{.AnsiColor.Black}}██{{.AnsiColor.White}}████{{.AnsiColor.Black}}████{{.AnsiColor.White}}████{{.AnsiColor.Black}}██{{.AnsiColor.BrightBlue}}████████████
+{{.AnsiColor.BrightBlue}}████████████████████████{{.AnsiColor.Black}}██████{{.AnsiColor.BrightBlue}}████{{.AnsiColor.Black}}██████{{.AnsiColor.BrightBlue}}████████████{{.AnsiColor.Black}}██████{{.AnsiColor.BrightBlue}}████{{.AnsiColor.Black}}██████{{.AnsiColor.BrightBlue}}████████████
+████████████████████████████████████████████████████████████████████████████████
+{{ .AnsiColor.Default }} {{.AnsiColor.BrightBlue}}███ BY ELMO9AWIMDZ DZ-HACK-LAB {{ .AnsiColor.Default }}`
+
+		banner.Init(colorable.NewColorableStdout(), isEnabled, isColorEnabled, bytes.NewBufferString(logo))
+
+        	fmt.Println("SEE USAGE ? ⚠️  ");
+		usage();
+        	os.Exit(1);
+    	}
+
+
+	max, _ := strconv.Atoi(os.Args[2])
+			
+
+	delay, _ := strconv.Atoi(os.Args[3])
+			
+
+	myCrawler := web.Crawler{ 
+			MaxConcurrencyLimit: max, 
+			StoragePath: "crawler/storage",  
+			CrawlDelay: delay,
+		}
+
+	// List of URLS to be crawled as a string array.
+	filePath := os.Args[1]
+        readFile, err := os.Open(filePath)
+  
+        if err != nil {
+                fmt.Println(err)
+        }
+        fileScanner := bufio.NewScanner(readFile)
+        fileScanner.Split(bufio.ScanLines)
+        var fileLines []string
+  
+        for fileScanner.Scan() {
+                fileLines = append(fileLines, fileScanner.Text())
+        }
+ 
+        readFile.Close()
+
+	save := flag.String("save", "report", "folder name to save on ")
+
+	for _, line := range fileLines {
+
+
+                fmt.Println(" 🤖 "+line)
+
+
+		notify.Alert("WEBCRAWLER", "CRAWLE ",line, "bug.png")
+				
+
+                alert.Error.Println("⚠️  ATTACK ON => 🤖 "+line)
+		fmt.Println(rainbow.Bold(rainbow.Hex("#8E44AD", "CRAWLER WORK ON : "+line)))
+		alert.Error.Println("⚠️  SAVE ON => 🤖 "+*save)
+		fmt.Printf("[%s] %s\n", aurora.Green("PASSED").String(),line)
+
+		urls := []string{line}
+		// Starting the crawler by passing the list of URLs.
+		myCrawler.Start(urls)
+		
+		notify.Alert("WEBCRAWLER", "DONE FOR ",line, "bug.png")
+				
+	}
+	
+	alert.Error.Println("⚠️  SAVE ON => 🤖 "+*save)
+			
+
+	notify.Alert("WEBCRAWLER", "SAVE ON ",*save, "bug.png")
+			
+
+	os.Rename("crawler",*save)
+}
